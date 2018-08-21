8746ccf9ead60e83c35623e4f10f17834f034380
crisbbi <christianbiasi@gmx.de>
2018-08-21 18:13:17 +0200

    added input/output generator of and-gate

--- javascripts/vhdl.js
+++ javascripts/vhdl.js
@@ -14,13 +14,46 @@ function generateCode(filename) {
     for(let index = 0; index < gates.length; index++) {
         switch(caption){
             case "and":
-                code = code.concat(" entity And_" + andCounter++ + " is");
+                // begin entity description
+                code = code.concat(" entity And_" + gates[index].inputCount + " is");
+                code = code.concat(" ports(");
+
+                /*  
+                add input ports:
+                count up until inputCount - 1 and then add the last 
+                port, to then attach ": STD_LOGIC;" to it
+                */
+                for(let inputNumber = 0; inputNumber < inputCount; inputNumber++) {
+                    if(inputNumber < inputCount - 1) {
+                        code = code.concat(" in" + inputNumber + ", ");
+                    } else {
+                        code = code.concat(" in" + inputNumber + ": STD_LOGIC;");
+                    }
+                }
+
+                /*  
+                add output ports:
+                count up until outputCount - 1 and then add the last 
+                port, to then attach ": STD_LOGIC;" to it
+                */
+                for(let outputNumber = 0; outputNumber < outputCount; outputNumber++) {
+                    if(outputNumber < inputCount - 1) {
+                        code = code.concat(" out" + outputNumber + ", ");
+                    } else {
+                        code = code.concat(" out" + outputNumber + ": STD_LOGIC;");
+                    }
+                }
+                
+                // close entity description
+                code = code.concat(" ); end And_" + gates[index].inputCount + ";");
+
+                // add architecture part
                 break;
             case "or":
-                code = code.concat(" entity Or_" + orCounter++ + " is");
+                code = code.concat(" entity Or_" + gates[index].inputCount + " is");
                 break;
             case "xor":
-                code = code.concat(" entity Xor_" + xorCounter++ + " is");
+                code = code.concat(" entity Xor_" + gates[index].inputCount + " is");
                 break;
             default: 
                 code = code;
