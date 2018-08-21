b195ed05b73f21197163fa8e956a152411c034ac
Christian Biasi <christianbiasi@gmx.de>
2018-08-21 14:58:18 +0200

    added counters for gates

--- javascripts/vhdl.js
+++ javascripts/vhdl.js
@@ -1,22 +1,29 @@
 // This file generates the VHDL code of the logic circuit as an export option.
 
 function generateCode(filename) {
-    var content = "";
+    var code = "";
+
+    var andCounter = 0;
+    var orCounter = 0;
+    var xorCounter = 0;
 
     // import library ieee to use 
-    content = content.concat("library ieee; use ieee.std_logic_1164.all; ");
+    code = code.concat("library ieee; use ieee.std_logic_1164.all;");
     
     // create an entity of the gates with I/O-ports
     for(let index = 0; index < gates.length; index++) {
         switch(caption){
             case "and":
+                code = code.concat(" entity And_" + andCounter++ + " is");
                 break;
             case "or":
+                code = code.concat(" entity Or_" + orCounter++ + " is");
                 break;
             case "xor":
+                code = code.concat(" entity Xor_" + xorCounter++ + " is");
                 break;
             default: 
-                content = content;
+                code = code;
         }
     }
 }
