6a1d204f4485eee14323c2b679b5ba0c8b0acae9
Christian Biasi <christianbiasi@gmx.de>
2018-08-22 20:46:01 +0200

    replaced code by codeArray to use saveStrings()

--- javascripts/vhdl.js
+++ javascripts/vhdl.js
@@ -1,94 +1,66 @@
 // This file generates the VHDL code of the logic circuit as an export option.
 
 function generateCode(filename) {
-    var code = "";
-
-    var andCounter = 0;
-    var orCounter = 0;
-    var xorCounter = 0;
+    // store generated code in an array so saveStrings() can be used
+    let codeArray = [];
 
     // import library ieee to use 
-    code = code.concat("library ieee; \n use ieee.std_logic_1164.all; \n");
+    codeArray.push("library ieee;"); 
+    codeArray.push("use ieee.std_logic_1164.all;");
     
     // create an entity of the gates with I/O-ports
     for(let index = 0; index < gates.length; index++) {
         switch(caption){
             case "and":
                 // begin entity description
-                code = code.concat("entity And_" + gates[index].inputCount + " is \n");
-                code = code.concat(" ports( \n");
+                codeArray.push("entity And_" + gates[index].inputCount + " is");
+                codeArray.push("ports(");
 
                 /*  
                 add input ports:
                 count up until inputCount - 1 and then add the last 
                 port, to then attach ": STD_LOGIC;" to it
                 */
+                let inputsString = "";
                 for(let inputNumber = 0; inputNumber < inputCount; inputNumber++) {
                     if(inputNumber < inputCount - 1) {
-                        code = code.concat(" in" + inputNumber + ", ");
+                        inputsString.concat("in" + inputNumber + ", ");
                     } else {
-                        code = code.concat(" in" + inputNumber + ": STD_LOGIC; \n");
+                        inputsString.concat(" in" + inputNumber + ": STD_LOGIC;");
                     }
                 }
+                codeArray.push(inputsString);
 
                 /*  
                 add output ports:
                 count up until outputCount - 1 and then add the last 
                 port, to then attach ": STD_LOGIC;" to it
                 */
-                for(let outputNumber = 0; outputNumber < outputCount; outputNumber++) {
-                    if(outputNumber < inputCount - 1) {
-                        code = code.concat(" out" + outputNumber + ", ");
-                    } else {
-                        code = code.concat(" out" + outputNumber + ": STD_LOGIC; \n");
-                    }
+                let outputsString = "";
+                for(let outputNumber = 0; outputNumber < inputCount; outputNumber++) {
+                   if(outputNumber < inputCount - 1) {
+                        outputsString.concat("out" + outputNumber + ", ");
+                   } else {
+                        outputsString.concat(" out" + outputNumber + ": STD_LOGIC;");
+                   }
                 }
+                codeArray.push(outputsString);
                 
                 // close entity description
-                code = code.concat(" ); \n end And_" + gates[index].inputCount + "; \n \n");
+                codeArray.push(");"); 
+                codeArray.push("end And_" + gates[index].inputCount + ";");
 
                 // add architecture part
                 break;
             case "or":
-                code = code.concat(" entity Or_" + gates[index].inputCount + " is");
-                code = code.concat(" ports( \n");
-
-                /*  
-                add input ports:
-                count up until inputCount - 1 and then add the last 
-                port, to then attach ": STD_LOGIC;" to it
-                */
-                for(let inputNumber = 0; inputNumber < inputCount; inputNumber++) {
-                    if(inputNumber < inputCount - 1) {
-                        code = code.concat(" in" + inputNumber + ", ");
-                    } else {
-                        code = code.concat(" in" + inputNumber + ": STD_LOGIC; \n");
-                    }
-                }
-
-                /*  
-                add output ports:
-                count up until outputCount - 1 and then add the last 
-                port, to then attach ": STD_LOGIC;" to it
-                */
-                for(let outputNumber = 0; outputNumber < outputCount; outputNumber++) {
-                    if(outputNumber < inputCount - 1) {
-                        code = code.concat(" out" + outputNumber + ", ");
-                    } else {
-                        code = code.concat(" out" + outputNumber + ": STD_LOGIC; \n");
-                    }
-                }
                 
-                // close entity description
-                code = code.concat(" ); \n end Or_" + gates[index].inputCount + "; \n \n");
-
-                // add architecture part
                 break;
             case "xor":
-                code = code.concat(" entity Xor_" + gates[index].inputCount + " is");
+                codeArray = codeArray.concat(" entity Xor_" + gates[index].inputCount + " is");
                 break;
             default: 
-                code = code;
+                codeArray = codeArray;
         }
     }
+    saveStrings(codeArray, filename, 'vhdl');
 }
