3b84a0766ab73200143af4916ec8523b4d42136e
Christian Biasi <christianbiasi@gmx.de>
2019-10-15 00:27:29 +0200

    added some explaining comment sections

--- javascripts/classes/changeWireConnectionChain.js
+++ javascripts/classes/changeWireConnectionChain.js
@@ -1,3 +1,19 @@
+/**
+ * In this file the connection type of three or four wire segments, that are connected
+ * in perpendicular form, as a "T" shape, or as a cross, changes with the
+ * execution of its corresponding concrete class methods, in case the given connection
+ * type fits the sublass. Otherwise the given connection point is passed to
+ * the next member of the chain.
+ * 
+ * author: Christian Biasi
+ * file created on 14.10.2019
+ */
+
+ /**
+  * The parent class, that provides the needed functionality (interface) for
+  * its subclasses to implement the Chain of responsibility pattern.
+  * 
+  */
 class WireConnectionTypeChain {
     changeConnectionType(connectionPoint, connectionType) {
     }
@@ -6,6 +22,10 @@ class WireConnectionTypeChain {
     }
 }
 
+/**
+ * The sublass, that changes the given connection point to a normal connection.
+ * It represents a single dot, that connects the wires.
+ */
 class NormalConnection extends WireConnectionTypeChain {
     changeConnectionType(connectionPoint, connectionType) {
         if(connectionType === "diodeConnection") {
@@ -21,6 +41,10 @@ class NormalConnection extends WireConnectionTypeChain {
     }
 }
 
+/**
+ * The sublass, that changes the given connection point to an empty connection.
+ * It represents no symbol, so that the wires are not connected.
+ */
 class NoConnection extends WireConnectionTypeChain {
     changeConnectionType(connectionPoint, connectionType) {
         if(connectionType === "normalConnection") {
@@ -30,12 +54,17 @@ class NoConnection extends WireConnectionTypeChain {
             this.nextchainMember.changeConnectionType(connectionPoint, connectionType);
         }
     }
-
+    
     setNextChainMember(nextchainMember) {
         this.nextchainMember = nextchainMember;
     }
 }
 
+/**
+ * The sublass, that changes the given connection point to a diode connection.
+ * It represents a diode symbol, where a horizontal wire also activates the
+ * connected vertical wire.
+ */
 class DiodeConnection extends WireConnectionTypeChain {
     changeConnectionType(connectionPoint, connectionType) {
         if(connectionType === "emptyConnection") {
@@ -45,7 +74,7 @@ class DiodeConnection extends WireConnectionTypeChain {
             this.nextchainMember.changeConnectionType(connectionPoint, connectionType);
         }
     }
-
+    
     setNextChainMember(nextchainMember) {
         this.nextchainMember = nextchainMember;
     }
