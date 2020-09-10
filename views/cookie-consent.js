292879b1fe6132b76f365b781896bce81ba6803e
Christian Biasi <christianbiasi@gmx.de>
2020-09-10 18:51:51 +0200

    extracted cookie consent, changed its background color

---
+++ views/cookie-consent.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function () {
+    window.cookieconsent.initialise({
+      "palette": {
+        "popup": {
+          "background": "#888",
+          "text": "#fff"
+        },
+        "button": {
+          "background": "#c83232",
+          "text": "#fff"
+        }
+      }
+    })
+});
