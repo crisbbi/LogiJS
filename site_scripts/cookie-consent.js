da19b93ce0bc483dc7c6c2c8bdce4f6fd6562ee9
Christian Biasi <christianbiasi@gmx.de>
2020-10-03 17:31:38 +0200

    updated cookie consent

--- site_scripts/cookie-consent.js
+++ site_scripts/cookie-consent.js
@@ -1,5 +1,9 @@
 window.addEventListener("load", function () {
     window.cookieconsent.initialise({
+      "position": "bottom-right",
+      "content": {
+        "dismiss": "Allow Cookies"
+      },
       "palette": {
         "popup": {
           "background": "#888",
